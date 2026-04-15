5083ef73ca997747780acc4c41fd635b40d13404
vkusnyjugolok <vkusnyjugolok@outlook.com>
2026-04-15 13:28:36 +0300

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,11 +1,11 @@
 <?php
-  $host = getenv('DB_HOST') ?: 'sql7.freesqldatabase.com';
-  $db   = getenv('DB_NAME') ?: 'sql7822094';
-  $user = getenv('DB_USER') ?: 'sql7822094';
-  $pass = getenv('DB_PASS') ?: 'a1jlKSQsAc'; 
+  $host = getenv('DB_HOST') ?: 'vkusnyjugolok-vkusnyjugolok-33e7.k.aivencloud.com';
+  $db   = getenv('DB_NAME') ?: 'defaultdb';
+  $user = getenv('DB_USER') ?: 'avnadmin';
+  $pass = getenv('DB_PASS') ?: 'AVNS_sanRGmQ_dvKptNwcXZL'; 
 
   try {
-      $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+      $pdo = new PDO("mysql:host=$host;port=23176;dbname=$db;charset=utf8", $user, $pass);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
       die("Ошибка подключения: " . $e->getMessage());
